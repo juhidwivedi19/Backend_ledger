a88eb3b78bf3d77781f8df50a85dccabd3a605e5
Juhi Dhar Dwivedi <257458510+juhidwivedi19@users.noreply.github.com>
2026-06-07 14:50:17 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -21,7 +21,7 @@ const userSchema= new mongoose.Schema({
         type:String,
         required:[true,"Password is required for creating an account"],
         minlength:[6,"Password should be at least 6 characters long"],
-        select:false //jab bhi user ko fetch karenge to password nahi aayega
+        select:false 
     },
 
     systemUser: {   
@@ -32,26 +32,26 @@ const userSchema= new mongoose.Schema({
     }
 
 }, {
-    timestamps:true  //createdAt aur updatedAt field automatically add ho jayenge
+    timestamps:true  
 })
 
 userSchema.pre("save",async  function(next){
 
     if(!this.isModified("password")){
-        return  //agar password modify nahi hua hai to next middleware pe chala jao
+        return  
     }
 
-    const hash=await bcrypt.hash(this.password,10)  //10 is the number of rounds for hashing, jitna zyada hoga utna secure hoga lekin time bhi zyada lagega
-     this.password=hash  //password ko hash karke save karenge database me
+    const hash=await bcrypt.hash(this.password,10)  
+     this.password=hash  
 
     return 
 })
 
 userSchema.methods.comparePassword= async function (password) {
 
-    // console.log(password, this.password)
+  
 
-    return await bcrypt.compare(password,this.password)  //jo password user ne login karte waqt diya hai usko database me stored hashed password se compare karenge, agar match karega to true return karega otherwise false
+    return await bcrypt.compare(password,this.password)  
 }
 
 const userModel =mongoose.model("user",userSchema)
